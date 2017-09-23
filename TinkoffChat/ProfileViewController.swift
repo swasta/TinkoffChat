0b23d1dbac4fd644208f7d38b1ee287800f99356
swasta <link.bor@gmail.com>
2017-09-23 16:45:52 +0300

    lesson_1

---
+++ TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  TinkoffChat
+//
+//  Created by Nikita Borodulin on 20/09/2017.
+//  Copyright © 2017 com.nikitaborodulin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
