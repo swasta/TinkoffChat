1eaf9e1a1e002a5fd582b0c5402a2cc6bf457ff6
swasta <link.bor@gmail.com>
2017-10-01 14:15:20 +0300

    lesson_2

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -2,46 +2,17 @@
 //  ProfileViewController.swift
 //  TinkoffChat
 //
-//  Created by Nikita Borodulin on 20/09/2017.
+//  Created by Nikita Borodulin on 27/09/2017.
 //  Copyright © 2017 com.nikitaborodulin. All rights reserved.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print(#function)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print(#function)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print(#function)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews() // default implementation does nothing
-        print(#function)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews() // default implementation does nothing
-        print(#function)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print(#function)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print(#function)
+    @IBAction func editAction(_ sender: UIButton) {
+        
     }
 }
